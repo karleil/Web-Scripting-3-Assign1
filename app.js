6b32fa94c9c61e878ae2901b2dac99b95c3802b8
karleil <karleillvillareal@gmail.com>
2025-02-17 01:31:42 -0800

    Comments 1

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
+//Karleil Villareal - A01397468
+//GitHub Link: https://github.com/karleil/Web-Scripting-3-Assign1
+const express = require('express'); //imports express module
 const app = express();
-const PORT = 3000;
-const booksRouter = require('./routes/Books');
-const bodyParser = require('body-parser');
+const PORT = 3000; //this will be the port number the server will listen to
+const booksRouter = require('./routes/Books'); //path to import books.js file
+const bodyParser = require('body-parser'); //imports body-parser module or the middleware 
 
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //parses the text as JSON using body-parser
 
-app.use('/api', booksRouter);
+app.use('/api', booksRouter); //this will be the path to access the booksRouter
  
-app.listen(PORT, () => {
+app.listen(PORT, () => { //listens to the port number / runs the server
     console.log(`Server running at http://localhost:${PORT}/`);
 });
